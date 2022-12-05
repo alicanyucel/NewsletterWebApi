8a87ed4dfdb134430d1bc0b82e14d2bd3201d810
ali can yücel <yucelalican@hotmail.com>
2022-12-05 20:46:11 +0300

    Haber bülteni get,getbyid,delete,post,update tamamlandı.

---
+++ NewsLetter/Newsletter.BackendApi/Controllers/NewsLetterController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newsletter.BackendApi.Models;
+
+namespace Newsletter.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsLetterController : ControllerBase
+    {
+        private readonly ContextDb _context;
+        public NewsLetterController(ContextDb context)
+        {
+            _context = context;
+
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _context.Users.FindAsync(id);
+            return Ok(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.Users.ToListAsync();
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(NewsLetter newsLetter)
+        {
+            await _context.Newsletters.AddAsync(newsLetter);
+            await _context.SaveChangesAsync();
+            return Ok("kayıt eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(NewsLetter newsLetter)
+        {
+            _context.Newsletters.Update(newsLetter);
+            await _context.SaveChangesAsync();
+            return Ok("kayıt güncellendi");
+
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _context.Newsletters.FindAsync(id);
+            _context.Newsletters.Remove(result);
+            await _context.SaveChangesAsync();
+            return Ok("kayıt silindi");
+
+        }
+    }
+}
--- NewsLetter/Newsletter.BackendApi/Program.cs
+++ NewsLetter/Newsletter.BackendApi/Program.cs
@@ -4,7 +4,13 @@ using Newsletter.BackendApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,7 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
