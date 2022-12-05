a3429e95a33d765bdfd0037c2d60fb6554deebda
ali can yücel <yucelalican@hotmail.com>
2022-12-05 18:47:27 +0300

    Kullanıcı ekleme tamamlandı.

---
+++ NewsLetter/Newsletter.BackendApi/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newsletter.BackendApi.Dtos;
+using Newsletter.BackendApi.Models;
+
+namespace Newsletter.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ContextDb _context;
+        public AuthController(ContextDb contextDb)
+        {
+            _context = contextDb;
+        }
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var IsUserNameExits = await _context.Users.Where(p => p.UserName == registerDto.UserName).FirstOrDefaultAsync();
+            if (IsUserNameExits != null)
+            {
+                return BadRequest("bu kullanici daha önce alınmış.");
+            };
+            var IsEmailExits = await _context.Users.Where(p => p.Email == registerDto.EmailAddress).FirstOrDefaultAsync();
+            if (IsEmailExits != null)
+            {
+                return BadRequest("bu email adresi daha önce alınmış.");
+            };
+            User user = new()
+            {
+                Email = registerDto.EmailAddress,
+                Password = registerDto.Password,
+                NameLastName = registerDto.NameLastName,
+                UserName = registerDto.UserName,
+
+            };
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok("kayit işlemi başarılı.");
+        }
+    }
+}
---
+++ NewsLetter/Newsletter.BackendApi/Dtos/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Newsletter.BackendApi.Dtos
+{
+    public class RegisterDto
+    {
+        public string NameLastName { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password{ get; set; }
+    }
+}
